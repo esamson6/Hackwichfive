d3ca13dc225ffe3690a02d75b4deed36891bfcf0
esamson6 <77295762+esamson6@users.noreply.github.com>
2021-02-20 15:07:35 -1000

    Completed Part 1-7

--- Hackwichfive/ViewController.swift
+++ Hackwichfive/ViewController.swift
@@ -8,12 +8,60 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+//declare and assign a global variable
+    var currentIndex = 0
+    //declare and assign a global variable that is an that holds a collection of strings that are names of our favorite food
+    
+    var favoriteFoodsArray = ["Steaks","Pasta","Breads","Fish","Pizza"]
+    
+    
+    
+    @IBOutlet weak var topLabel: UILabel!
+    @IBOutlet weak var bottomLabel: UILabel!
+    
+    //IBOutlet for button to access it's properties -- change its attributes
+    
+    @IBAction func buttonLabel(_ sender: Any) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
-
-
+        
+        topLabel.text = " My Favorite Foods"
+        //set bottomLabel to display the item at "currentIndex" upon launch of app
+        
+        bottomLabel.text=favoriteFoodsArray[currentIndex]
 }
 
+//Part 8
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+    
+        if self.currentIndex < self.favoriteFoodsArray.count
+            {
+        //you could also put the integer (if self.currentIndex < 5)
+        //set bottomLabeltext to item in the array at currentIndex
+         
+            bottomLabel.text = favoriteFoodsArray[currentIndex]
+            //set the title of the button to "Next"
+            
+            
+    }
+     else
+    {
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+    
+   
+        
+        
+}
+
+}
