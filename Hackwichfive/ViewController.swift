8ce18f7067c14eb9c56364479767523d0d9c2420
esamson6 <77295762+esamson6@users.noreply.github.com>
2021-02-21 08:37:35 -1000

    Made changes and corrections 2/21

--- Hackwichfive/ViewController.swift
+++ Hackwichfive/ViewController.swift
@@ -13,55 +13,50 @@ class ViewController: UIViewController {
     //declare and assign a global variable that is an that holds a collection of strings that are names of our favorite food
     
     var favoriteFoodsArray = ["Steaks","Pasta","Breads","Fish","Pizza"]
-    
-    
-    
+
     @IBOutlet weak var topLabel: UILabel!
     @IBOutlet weak var bottomLabel: UILabel!
     
     //IBOutlet for button to access it's properties -- change its attributes
     
-    @IBAction func buttonLabel(_ sender: Any) {
-    }
+    @IBOutlet weak var buttonLabel: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        topLabel.text = " My Favorite Foods"
+        topLabel.text = "My Favorite Foods"
         //set bottomLabel to display the item at "currentIndex" upon launch of app
-        
-        bottomLabel.text=favoriteFoodsArray[currentIndex]
-}
+        //bottom label will display the array in the order it it is listed 
+        bottomLabel.text=favoriteFoodsArray[currentIndex]   }
 
 //Part 8
     
+    //When button is pressed the favorite foods array will be displayed
+    //Pressing the button will allow you to go through the array
     @IBAction func buttonPressed(_ sender: Any) {
     
-        if self.currentIndex < self.favoriteFoodsArray.count
-            {
+        if self.currentIndex < self.favoriteFoodsArray.count {
         //you could also put the integer (if self.currentIndex < 5)
+            
         //set bottomLabeltext to item in the array at currentIndex
-         
-            bottomLabel.text = favoriteFoodsArray[currentIndex]
+         bottomLabel.text = favoriteFoodsArray[currentIndex]
             //set the title of the button to "Next"
+            buttonLabel.setTitle ("Next", for: UIControl.State.normal)
+            currentIndex+=1
             
             
     }
      else
     {
-        
-        
-        
-        
+    //Once all the items have been displayed in the array it will be button will be disable
+    print("Button has been disabled")
+        (buttonLabel!).isEnabled=false
         
         
         
     }
     
-    
-   
-        
-        
-}
+    }
 
 }
